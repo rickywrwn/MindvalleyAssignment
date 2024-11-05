a53ffe1b27a73c10907a89b9b2ef99b4cef19cc2
Ricky Wirawan <rickywrwn@gmail.com>
2024-11-05 19:34:49 +0700

    add custom font (roboto)

---
+++ MindvalleyAssignment/Common/Extensions/UIFont+CustomFonts.swift
@@ -0,0 +1,21 @@
+//
+//  UIFont+CustomFonts.swift
+//  MindvalleyAssignment
+//
+//  Created by ricky wirawan on 05/11/24.
+//
+
+import UIKit
+
+
+extension UIFont{
+    
+    public enum RobotoType: String {
+        case regular = "-Regular"
+        case bold = "-Bold"
+    }
+    
+    static func roboto(type: RobotoType, size: CGFloat) -> UIFont?{
+        return UIFont(name: "Roboto\(type.rawValue)", size: size)
+    }
+}

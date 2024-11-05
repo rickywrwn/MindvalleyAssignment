07ec46329749b15084ba8aa9c910d4c2373c0fe3
Ricky Wirawan <rickywrwn@gmail.com>
2024-11-05 07:27:58 +0700

    Initial Commit

---
+++ MindvalleyAssignmentTests/MindvalleyAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindvalleyAssignmentTests.swift
+//  MindvalleyAssignmentTests
+//
+//  Created by ricky wirawan on 05/11/24.
+//
+
+import Testing
+@testable import MindvalleyAssignment
+
+struct MindvalleyAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
